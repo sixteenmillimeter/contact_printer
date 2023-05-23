da69bf747e38b10ce75e7348771b2209ec216508
mattmcw <matt@sixteenmillimeter.com>
2023-05-23 11:50:15 -0400

    Build and canonicalize all stl files from scad file. Autogenerate list of models from scad if statement at end of script

--- build.sh
+++ build.sh
@@ -6,8 +6,14 @@ reset=`tput sgr0`
 echo "Building ${green}contact_printer${reset} project..."
 #build OpenSCAD models
 
+mkdir -p stl
+
+cat scad/contact_printer.scad | grep "PART ==" | awk -F'==' '{print $2}' | awk -F'"' '{print $2}' | awk -F'"' '{print $1}' | sort > models.txt
+
 while read m; do
-	echo $m
+	echo "Rendering $m..."
+	openscad -o "stl/contact_printer_$m.stl" -D "PART=\"$m\"" scad/contact_printer.scad
+	python scad/common/c14n_stl.py "stl/contact_printer_$m.stl"
 done < models.txt
 
 #run client tests?
