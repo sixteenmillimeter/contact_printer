fe532a1f65371fac854cd1762abc35eb6cff990b
mattmcw <matt@sixteenmillimeter.com>
2024-02-11 10:11:53 -0500

    Start claiming pins

--- ino/contact_printer/contact_printer.ino
+++ ino/contact_printer/contact_printer.ino
@@ -1,5 +1,28 @@
 #include "ContactPrinter.h";
 
+/**
+ * 
+ * 
+ * Target Board: ESP32 Dev Kit
+ * 
+ * Pins
+ * 
+ *  7 Takeup Picture Enable
+ *  8 Takeup Picture Clockwise
+ *  9 Takeup Picture Counter Clockwise
+ * 
+ * 12 Takeup Stock Enable - set duty speed
+ * 10 Takeup Stock Clockwise
+ * 11 Takeup Stock Counter Clockwise
+ * 
+ * 13 Drive Enable
+ * 12 Drive Forward (Clockwise)
+ * 14 Drive Backward (Counter Clockwise)
+ * 27 Drive Encoder A
+ * 26 Drive Encoder B
+ * 
+ **/
+
 ContactPrinter contact_printer;
 
 void setup () {
