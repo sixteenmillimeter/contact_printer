f16d0597ec463acfe660e3a38abf776991af8fd0
mattmcw <matt@sixteenmillimeter.com>
2024-10-09 02:03:07 -0400

    Update esp32 neopixel demo script

--- ino/demos/esp32_neopixel/esp32_neopixel.ino
+++ ino/demos/esp32_neopixel/esp32_neopixel.ino
@@ -4,28 +4,16 @@
 #define PIN 15
 #define NUM 1
 
-int r = 255;
-int g = 255;
-int b = 255;
-
-//neopixel jewel 7 = NEO_GRBW + NEO_KHZ800
-//neopixel strips = NEO_GRB + NEO_KHZ800
-Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUM, PIN, NEO_GRBW + NEO_KHZ800);
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUM, PIN, NEO_GRB + NEO_KHZ800);
 
 void setup() {
 	Serial.begin(115200);
 	pixels.begin();
-	pixels.setBrightness(55);
-	for ( int i = 0; i < NUM; i++ ) {
-       pixels.setPixelColor(i, 0, 0, 0);
-    }
+	pixels.setBrightness(255);
+	pixels.fill(0xFF0000);
 	pixels.show();
 }
 
 void loop () {
-   for ( int i = 0; i < NUM; i++ ) {
-       pixels.setPixelColor(i, r, g, b);
-   }
-   pixels.show();
-   delay(300);
+	pixels.show();
 }
